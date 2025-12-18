cf61b44d009db88381c11345a3f8529beef89eb6
Aarón <apovjoa@upv.edu.es>
2025-12-18 15:49:16 +0100

    Update TableEntry.h

--- TableEntry.h
+++ TableEntry.h
@@ -6,47 +6,54 @@
 
 template <typename V> 
 class TableEntry {
-    public:
-   
-    std::string key; //El elemento clave del par. 
-    V value; //El elemento valor del par
+public:
+    std::string key; // Clave del par
+    V value;         // Valor del par
 
-TableEntry(std::string key, V value){
-    
-    this->key = key;
-    this->value = value;
- 
-}
-TableEntry(std::string key) {    
-           
-    this->key=key;
-    this->value = V();
+    TableEntry(std::string key, V value) {
+        this->key = key;
+        this->value = value;
+    }
 
-}
-TableEntry(){
+    TableEntry(std::string key) {    
+        this->key = key;
+        this->value = V();
+    }
 
-    this->key=("");
-    this->value = V();
-}
+    TableEntry() {
+        this->key = "";
+        this->value = V();
+    }
 
-friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-   
-    return te1.key==te2.key;
+    // ------------------ OPERADORES ------------------
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key == te2.key;
+    }
 
-}
-friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key != te2.key;
+    }
 
-return te1.key!=te2.key;
+    // Comparación por clave para ABB
+    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key < te2.key;
+    }
 
-}
+    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key > te2.key;
+    }
 
-friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-
- out << te.key << " -> " << te.value;
+    // Impresión
+    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+        out << te.key << " -> " << te.value;
         return out;
+    }
+};
 
-}
+#endif
+
 
 
 };
+
 #endif
